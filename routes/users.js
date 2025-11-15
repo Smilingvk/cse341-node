136485fdf9186345a8fcc97b5db502d1c0430fd0
Smilingvk <valeriaguzmanavalos@gmail.com>
2025-11-15 15:16:18 -0500

    all  update

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,7 @@ const ctrl = require('../controllers/users');
 
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getSingle);
-router.post('/', ctrl.createUser);
 router.put('/:id', ctrl.updateUser);
 router.delete('/:id', ctrl.deleteUser);
-
+router.post('/', ctrl.createUser);
 module.exports = router;
--- server.js
+++ server.js
@@ -19,20 +19,6 @@ app.use('/users', usersRouter);
 
 // 404 fallback
 app.use((req,res) => res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`));
-const session = require('express-session');
-const passport = require('./auth'); // archivo anterior
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/auth', require('./routes/auth'));
-
 mongodb.initDb((err) => {
   if (err) {
     console.error('DB init error:', err);
